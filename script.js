b7d9525d64c0ccde7fceeb4fe277f99f97692c88
ksanthoshfsd <ksanthoshfsd@gmail.com>
2024-03-31 21:51:52 +0530

    Practice 3

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 var a = ["abc","xyz","ab"];
 var [c,d,e] = a
 console.log(c,d,e)
+// Define a constructor function for creating objects of type 'Person'
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  
+  // Add a method to the Person prototype
+  Person.prototype.sayHello = function() {
+    console.log("Hello, my name is " + this.name + " and I'm " + this.age + " years old.");
+  };
+  
+  // Create a new instance of Person
+  var person1 = new Person("Alice", 30);
+  var person2 = new Person("Bob", 25);
+  
+  // Access object properties
+  console.log(person1.name); // Output: Alice
+  console.log(person2.age);  // Output: 25
+  
+  // Call object method
+  person1.sayHello(); // Output: Hello, my name is Alice and I'm 30 years old.
+  person2.sayHello(); // Output: Hello, my name is Bob and I'm 25 years old.
+  
